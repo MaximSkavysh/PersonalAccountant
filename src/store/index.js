76814eb0d3f30a1a2ea9047d59f69a2fbe959baa
Maksim Skavysh <SkavyshM@iba>
2020-07-06 15:14:46 +0300

    add username and currnency

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,40 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import auth from './auth'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import auth from './auth';
+import info from './info';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    error: null    
+    error: null,
+    currncyData: {}
   },
   mutations: {
-    setError(state, error){
-      state.error = error
+    setError(state, error) {
+      state.error = error;
     },
-    clearError(state){
-      state.error = null
+    clearError(state) {
+      state.error = null;
+    },
+    setCurrData(state, currncyData) {
+      state.currncyData = currncyData;
     }
   },
-  getters:{
-    error: s => s.error
+  getters: {
+    error: s => s.error,
+    currncyData: s => s.currncyData
   },
   actions: {
+    async fetchCurrency({ commit }) {
+      const res = await fetch(
+        `https://api.exchangeratesapi.io/latest?symbols=USD,BRL,GBP`
+      );
+      commit('setCurrData', await res.json());
+    }
   },
   modules: {
-    auth
+    auth,
+    info
   }
-})
+});
